f58dd01f9be556d3ad1f5e5b8a31c96f873e1c9f
cyxstart <cyxstart@126.com>
2023-01-26 12:32:41 +0800

    222

--- src/main/java/com/slow/Customer.java
+++ src/main/java/com/slow/Customer.java
@@ -1,5 +1,7 @@
 package com.slow;
 
+import java.util.Scanner;
+
 /**
  * ClassName: Customer
  * Package: com.slow
@@ -12,6 +14,9 @@ package com.slow;
  */
 public class Customer {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入姓名：");
+        String name = scanner.next();
+        System.out.println(name);
     }
 }
