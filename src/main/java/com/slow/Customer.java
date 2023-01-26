6eb300192948506d17e85c0a482b0a05ee1b0b79
cyxstart <cyxstart@126.com>
2023-01-26 12:35:41 +0800

    222

--- src/main/java/com/slow/Customer.java
+++ src/main/java/com/slow/Customer.java
@@ -18,5 +18,8 @@ public class Customer {
         System.out.println("请输入姓名：");
         String name = scanner.next();
         System.out.println(name);
+        System.out.println("密码：");
+        String password = scanner.next();
+        System.out.println("password = " + password);
     }
 }
