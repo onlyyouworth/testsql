47ac9b61ee605f34c90773986b550ff393250aee
cyxstart <cyxstart@126.com>
2023-01-26 12:43:08 +0800

    2222

--- src/main/java/com/slow/Customer.java
+++ src/main/java/com/slow/Customer.java
@@ -19,8 +19,14 @@ public class Customer {
         String name = scanner.next();
         System.out.println(name);
         System.out.println("请输入密码：");
+        System.out.println("hh密码：");
+        System.out.println("请输入密码：");
         String password = scanner.next();
         System.out.println("password = " + password);
+        System.out.println("不好意思，密码错误");
+        System.out.println("不好意思，密码错误");
+        System.out.println("不好意思，密码错误");
+        System.out.println("不好意思，密码错误");
         System.out.println("你好！" + name + "欢迎来到Java世界！");
         System.out.println("你好！" + name + "欢迎来到Java世界！");
         System.out.println("你好！" + name + "欢迎来到Java世界！");
