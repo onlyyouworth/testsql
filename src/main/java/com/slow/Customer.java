4a577f9f1353f6728b94fb053f9f12b94226af72
onlyyouworth <81966541+onlyyouworth@users.noreply.github.com>
2023-01-26 13:42:21 +0800

    Update Customer.java

--- src/main/java/com/slow/Customer.java
+++ src/main/java/com/slow/Customer.java
@@ -31,6 +31,7 @@ public class Customer {
         System.out.println("你好！" + name + "欢迎来到Java世界！");
         System.out.println("你好！" + name + "欢迎来到Java世界！");
         System.out.println("push");
+        //修改
 
 
     }
